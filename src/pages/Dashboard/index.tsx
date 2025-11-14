872551cd57479a7b790771d22c38952a8a932f5c
akristina <kristin07.96@gmail.com>
2025-11-14 13:14:27 +0400

    add colors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,10 @@
 const Dashboard = () => (
   <div className="Dashboard container mx-auto px-4">
       Dashboard content here
+      Color pallate
+      <div className="bg-primary text-txt"> This is primary background with text</div>
+      <div className="bg-secondary text-error"> This is secondary background with text error</div>
+      <div className="bg-disabled text-warning"> This is disabled background with text warning</div>
   </div>
 );
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import Quote from '../../components/shareds/Quote';
 
 const Home = () => (
-  <div className="Home container mx-auto px-4 bg-primary">
+  <div className="Home container mx-auto px-4 bg-primary-900 text-txt">
     <ul>
       <li>
         <Link to="/about">About</Link>
